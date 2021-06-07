b5088767e13abaac2ec990123b10638fb4acb6c9
Nayanda Haberty <nayanda1@outlook.com>
2021-06-07 11:57:30 +0700

    Update Draftsman to 1.0.7

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Quick/Quick.git", from: "4.0.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "9.2.0"),
-        .package(url: "https://github.com/nayanda1/Draftsman.git", from: "1.0.6"),
+        .package(url: "https://github.com/nayanda1/Draftsman.git", from: "1.0.7"),
         .package(url: "https://github.com/nayanda1/Pharos.git", from: "1.1.5")
     ],
     targets: [
