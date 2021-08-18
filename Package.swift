f646d0c3e3627d4724d29005083b976be6080374
Nayanda Haberty <nayanda1@outlook.com>
2021-08-18 08:56:57 +0700

    Add builder package dependency

--- Package.swift
+++ Package.swift
@@ -17,13 +17,14 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Quick/Quick.git", from: "4.0.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "9.2.0"),
-        .package(url: "https://github.com/hainayanda/Draftsman.git", from: "2.0.2"),
-        .package(url: "https://github.com/hainayanda/Pharos.git", from: "1.2.3")
+        .package(url: "https://github.com/hainayanda/Draftsman.git", from: "2.0.3"),
+        .package(url: "https://github.com/hainayanda/Pharos.git", from: "1.2.3"),
+        .package(url: "https://github.com/hainayanda/Builder.git", from: "1.0.1")
     ],
     targets: [
         .target(
             name: "Artisan",
-            dependencies: ["Draftsman", "Pharos"],
+            dependencies: ["Draftsman", "Pharos", "Builder"],
             path: "Artisan/Classes"
         ),
         .testTarget(
